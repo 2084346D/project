3c8c9a939380aedd0a893d41c83ce8cda935d784
2084346D <2084346D@student.gla.ac.uk>
2016-11-09 20:32:56 +0000

    some files did not add in last commit, should be fixed now

--- tennis_system_project/tennis/urls.py
+++ tennis_system_project/tennis/urls.py
@@ -3,4 +3,5 @@ from tennis import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about', views.about, name='about'),
 ]
--- tennis_system_project/tennis/views.py
+++ tennis_system_project/tennis/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # testing a simple view
 def index(request):
-	return HttpResponse("This is a test")
+	context_dict = {'boldmessage': "Here's a message"}
+	return render(request, 'tennis/index.html', context=context_dict)
+
+def about(request):
+	return HttpResponse("This is an about page <br/> <a href='/tennis/'> Home </a>")
 
