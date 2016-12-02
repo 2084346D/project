b5b5f35137e7003d3e10321e015ac266b0612b82
2084346D <2084346D@student.gla.ac.uk>
2016-12-02 22:03:02 +0000

    basic user profiles added

--- tennis_system_project/tennis/urls.py
+++ tennis_system_project/tennis/urls.py
@@ -4,4 +4,5 @@ from tennis import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name='about'),
+    url(r'^register/$', views.register, name='register'),
 ]
